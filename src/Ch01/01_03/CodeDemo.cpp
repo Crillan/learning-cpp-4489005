a5c4860bf67ccf969aab5b8bcc16cf47a0585661
Crillan <dandlforever@hotmail.com>
2024-06-13 18:54:02 +0000

    Committing the first few lessons.

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,8 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "Please enter your user name:" << std::endl << std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Thank you, " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
